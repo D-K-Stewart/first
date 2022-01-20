0b45b65781a5f1ee59dcc6565988c67818a506c6
D-K-Stewart <90126394+D-K-Stewart@users.noreply.github.com>
2022-01-19 16:57:45 -0800

    Initial commit

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+	public static void main(String[] args) {
+		System.out.println("My name is David");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Puyallup, WA");
+
+	}
+}
